cb2eb9edd9954e9be4a576d49a33ffdc3f25e129
WilmarDeML <carewaz23@hotmail.com>
2025-03-09 14:59:10 -0500

    solve exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const showWhenVisible = { display: isVisible ? '' : 'none' }
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsVisible(!isVisible)}>{isVisible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url} <br />
+        likes: {blog.likes} <button>like</button> <br />
+        {blog.user?.name ?? 'anonymous'} <br />
+      </div>
+    </div>
+  )
+}
 
 export default Blog
