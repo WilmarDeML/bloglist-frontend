85fdedf98f4e3ca38b9b3af6ba6422c49db07a42
WilmarDeML <carewaz23@hotmail.com>
2025-03-09 14:21:56 -0500

    solve exercise 5.6

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,37 +1,78 @@
-const BlogForm = ({ addBlog, title, setTitle, author, setAuthor, url, setUrl }) => (
-  <form onSubmit={addBlog}>
-    <h2>create new</h2>
-    <div style={{display: 'flex', gap: .5 +'em'}}>
-      <label htmlFor="title">title</label>
-      <input
-        type="text"
-        value={title}
-        name="Username"
-        onChange={({ target }) => setTitle(target.value)}
-      />
-    </div>
+import { useState, useRef } from 'react'
+
+import blogService from '../services/blogs'
+
+import Togglable from './Togglable'
+
+const BlogForm = ({ blogs, setBlogs, showNotification }) => {
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const noteFormRef = useRef(null)
+
+  const handleBlogSubmit = async (event) => {
+    event.preventDefault()
     
-    <div style={{display: 'flex', gap: .5 +'em'}}>
-      <label htmlFor="author">author</label>
-      <input
-        type="text"
-        value={author}
-        name="Username"
-        onChange={({ target }) => setAuthor(target.value)}
-      />
-    </div>
-    
-    <div style={{display: 'flex', gap: .5 +'em'}}>
-      <label htmlFor="url">url</label>
-      <input
-        type="text"
-        value={url}
-        name="Username"
-        onChange={({ target }) => setUrl(target.value)}
-      />
-    </div>
-    <button type="submit">create</button>
-  </form>
-)
+    const blog = {
+      title, author, url
+    }
+
+    try {
+      const createdBlog = await blogService.create(blog)
+  
+      noteFormRef.current.toggleVisibility()
+
+      setBlogs(blogs.concat(createdBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      showNotification(`a new blog '${blog.title}' added`)
+    } catch (error) {
+      const errorMessage = error.response?.data?.error ?? 'error saving the blog'
+      showNotification(errorMessage, error)
+      console.error(error.response?.data?.error ?? error.message)
+    }
+  }
+
+  return (
+    <Togglable buttonLabel="new blog" ref={noteFormRef}>
+      <form onSubmit={handleBlogSubmit}>
+        <h2>create new</h2>
+        <div style={{display: 'flex', gap: .5 +'em'}}>
+          <label htmlFor="title">title</label>
+          <input
+            type="text"
+            value={title}
+            name="Username"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        
+        <div style={{display: 'flex', gap: .5 +'em'}}>
+          <label htmlFor="author">author</label>
+          <input
+            type="text"
+            value={author}
+            name="Username"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        
+        <div style={{display: 'flex', gap: .5 +'em'}}>
+          <label htmlFor="url">url</label>
+          <input
+            type="text"
+            value={url}
+            name="Username"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </Togglable>
+  )
+}
 
 export default BlogForm
--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,42 +1,9 @@
-import { useState, useRef } from 'react'
-
-import blogService from '../services/blogs'
 
 import Blog from './Blog'
 import BlogForm from './BlogForm'
 import Notification from './Notification'
-import Togglable from './Togglable'
 
 const BlogList = ({ logout, blogs, setBlogs, name, showNotification, error, notificationMessage }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const noteFormRef = useRef(null)
-  
-  const handleBlogSubmit = async (event) => {
-    event.preventDefault()
-    
-    const blog = {
-      title, author, url
-    }
-
-    try {
-      const createdBlog = await blogService.create(blog)
-  
-      noteFormRef.current.toggleVisibility()
-
-      setBlogs(blogs.concat(createdBlog))
-      setTitle('')
-      setAuthor('')
-      setUrl('')
-      showNotification(`a new blog '${blog.title}' added`)
-    } catch (error) {
-      const errorMessage = error.response?.data?.error ?? 'error saving the blog'
-      showNotification(errorMessage, error)
-      console.error(error.response?.data?.error ?? error.message)
-    }
-  }
 
   return (
     <div>
@@ -46,13 +13,7 @@ const BlogList = ({ logout, blogs, setBlogs, name, showNotification, error, noti
 
       <p>{name} logged in <button onClick={logout}>logout</button></p>
 
-      <Togglable buttonLabel="new blog" ref={noteFormRef}>
-        <BlogForm addBlog={handleBlogSubmit}
-          title={title} setTitle={setTitle}
-          author={author} setAuthor={setAuthor}
-          url={url} setUrl={setUrl}
-        />
-      </Togglable>
+      <BlogForm blogs={blogs} setBlogs={setBlogs} showNotification={showNotification} />
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
